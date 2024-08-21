f7155d1d2850d2e3f7885c4fb99974a45d902517
David B. <babel.david@gmail.com>
2024-08-21 23:08:27 +0200

    load toc faste

--- auto-toc.js
+++ auto-toc.js
@@ -1,22 +1,19 @@
-let toc = document.createElement("div");
-toc.classList.add("toc");
-document.body.insertBefore(toc, document.body.firstChild);
-
 let tocScript = document.createElement("script");
 tocScript.src =
   "https://cdnjs.cloudflare.com/ajax/libs/tocbot/4.27.4/tocbot.min.js";
 document.body.appendChild(tocScript);
-
-Array.from(document.querySelectorAll("h2")).forEach((el) => {
-  el.id = el.innerHTML;
-});
+let toc = document.createElement("div");
+toc.classList.add("toc");
+document.body.insertBefore(toc, document.body.firstChild);
 
 document.addEventListener("DOMContentLoaded", () => {
-  setTimeout(() => {
-    tocbot.init({
-      tocSelector: ".toc",
-      contentSelector: "body",
-      headingSelector: "h2, h3",
-    });
-  }, 2000);
+  Array.from(document.querySelectorAll("h2")).forEach((el) => {
+    el.id = el.innerHTML;
+  });
+
+  tocbot.init({
+    tocSelector: ".toc",
+    contentSelector: "body",
+    headingSelector: "h2, h3",
+  });
 });
